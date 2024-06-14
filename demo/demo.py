4921953d3c5bf3416144f449f9a2baa97743ddc8
server26 <jiayir2@126.com>
2024-06-14 11:35:35 +0800

    Update demo

--- demo/demo.py
+++ demo/demo.py
@@ -31,19 +31,17 @@ import numpy as np
 import torch
 
 
-# constants
-WINDOW_NAME = "PlaneRecTR demo"
-
-
-def get_coordinate_map(K, device, h=192, w=256):
+def get_coordinate_map(K, device, h=192, w=256, oh=480, ow=640):
+    # Calculate K_inv * xy1, taking into account that the image has been scaled (oh/ow->h/w), 
+    # and if there are any other image processing steps, they should be included in the calculation.
     K_inv = np.linalg.inv(np.array(K))
 
     K = torch.FloatTensor(K).to(device)
     K_inv = torch.FloatTensor(K_inv).to(device)
 
 
-    x = torch.arange(w, dtype=torch.float32).view(1, w) / w * 640
-    y = torch.arange(h, dtype=torch.float32).view(h, 1) / h * 480
+    x = torch.arange(w, dtype=torch.float32).view(1, w) / w * ow
+    y = torch.arange(h, dtype=torch.float32).view(h, 1) / h * oh
 
     x = x.to(device)
     y = y.to(device)
@@ -56,6 +54,32 @@ def get_coordinate_map(K, device, h=192, w=256):
 
     return k_inv_dot_xy1
 
+# def resize_image(img):
+#     h, w = img.shape[:2]
+#     if w/h == 1.5:
+#         re_img = cv2.resize(img, (256, 192))
+    
+#     elif w/h > 1.5:
+#         re_w = 256
+#         re_h = int(re_w/w*h)
+#         pad_h = 192-re_h
+#         pad_h1 = int(pad_h/2)
+#         pad_h2 = pad_h - pad_h1
+#         re_img = cv2.resize(img, (re_w, re_h))
+#         re_img = cv2.copyMakeBorder(re_img, top = pad_h1, bottom = pad_h2, left = 0, right = 0,
+#                                     borderType = cv2.BORDER_CONSTANT, value = 0)
+#     else:
+#         re_h = 192
+#         re_w = int(re_h/h*w)
+#         pad_w = 256-re_w
+#         pad_w1 = int(pad_w/2)
+#         pad_w2 = pad_w - pad_w1
+#         re_img = cv2.resize(img, (re_w, re_h))
+#         re_img = cv2.copyMakeBorder(re_img, top = 0, bottom = 0, left = pad_w1, right = pad_w2,
+#                                     borderType = cv2.BORDER_CONSTANT, value = 0)
+        
+#     return re_img
+        
 
 def setup_cfg(args):
     # load config from file and command-line arguments
@@ -76,8 +100,6 @@ def get_parser():
         metavar="FILE",
         help="path to config file",
     )
-    parser.add_argument("--webcam", action="store_true", help="Take inputs from webcam.")
-    parser.add_argument("--video-input", help="Path to video file.")
     parser.add_argument(
         "--input",
         nargs="+",
@@ -89,40 +111,42 @@ def get_parser():
         help="A file or directory to save output visualizations. "
         "If not given, will show output in an OpenCV window.",
     )
-
-    parser.add_argument(
-        "--confidence-threshold",
-        type=float,
-        default=0.5,
-        help="Minimum score for instance predictions to be shown",
-    )
-
     parser.add_argument(
         "--fx",
         type=float,
-        # default=0.5,
         help="camera K",
     )
 
     parser.add_argument(
         "--fy",
         type=float,
-        # default=0.5,
         help="camera K",
     )
 
     parser.add_argument(
         "--ox",
         type=float,
-        # default=0.5,
         help="camera K",
     )
 
     parser.add_argument(
         "--oy",
         type=float,
-        # default=0.5,
         help="camera K",
+    )
+
+    parser.add_argument(
+        "--original-w",
+        type=float,
+        default=640,
+        help="original width corresponding to camera K",
+    )
+
+    parser.add_argument(
+        "--original-h",
+        type=float,
+        default=480,
+        help="original height corresponding to camera K",
     )
 
     parser.add_argument(
@@ -132,24 +156,6 @@ def get_parser():
         nargs=argparse.REMAINDER,
     )
     return parser
-
-
-def test_opencv_video_format(codec, file_ext):
-    with tempfile.TemporaryDirectory(prefix="video_format_test") as dir:
-        filename = os.path.join(dir, "test_file" + file_ext)
-        writer = cv2.VideoWriter(
-            filename=filename,
-            fourcc=cv2.VideoWriter_fourcc(*codec),
-            fps=float(30),
-            frameSize=(10, 10),
-            isColor=True,
-        )
-        [writer.write(np.zeros((10, 10, 3), np.uint8)) for _ in range(30)]
-        writer.release()
-        if os.path.isfile(filename):
-            return True
-        return False
-
 
 if __name__ == "__main__":
     mp.set_start_method("spawn", force=True)
@@ -167,37 +173,30 @@ if __name__ == "__main__":
             args.input = glob.glob(os.path.expanduser(args.input[0]))
             assert args.input, "The input path(s) was not found"
         for path in tqdm.tqdm(args.input, disable=not args.output):
-            # use PIL, to be consistent with evaluation
-            img = read_image(path, format="BGR")
+            img = read_image(path, format="RGB")
+            img = cv2.resize(img, (256, 192))
+
             start_time = time.time()
             predictions = demo.run_on_image(img)
-            sem_seg = predictions["sem_seg"].argmax(dim=0).cpu() # torch.Size([480, 640]) # sem_seg 21, 192, 256
-            pred = np.array(sem_seg, dtype=np.int) # (480, 640)
+
+            sem_seg = predictions["sem_seg"].argmax(dim=0).cpu() # torch.Size([192, 256]) # sem_seg 21, 192, 256
+            pred = np.array(sem_seg, dtype=np.int) # (192, 256)
             plane_depth = predictions["planes_depth"].cpu().numpy()
             K = [[args.fx, 0, args.ox],
             [0, args.fy, args.oy],
             [0, 0, 1]]
             vis_dicts = {
-                        'image': img,
+                        'image': img[:,:,::-1], #->BGR
                         'segmentation': pred,
                         'depth_predplane': plane_depth,
-                        'K_inv_dot_xy_1': get_coordinate_map(K, torch.device("cpu"), h=192, w=256).numpy(),
+                        # Calculate K_inv * xy1, taking into account that the image has been scaled (oh/ow->h/w), 
+                        # and if there are any other image processing steps, they should be included in the calculation.
+                        'K_inv_dot_xy_1': get_coordinate_map(K, torch.device("cpu"), h=192, w=256, oh=args.original_h, ow=args.original_w).numpy(),
                     }
-            # logger.info(
-            #     "{}: {} in {:.2f}s".format(
-            #         path,
-            #         "detected {} instances".format(len(predictions["instances"]))
-            #         if "instances" in predictions
-            #         else "finished",
-            #         time.time() - start_time,
-            #     )
-            # )
 
             if args.output:
                 if os.path.isdir(args.output):
-                    # assert os.path.isdir(args.output), args.output
                     output_root = args.output
-                    # out_filename = os.path.join(args.output, os.path.basename(path))
                     
                 else:
                     assert len(args.input) == 1, "Please specify a directory with args.output"
@@ -208,62 +207,3 @@ if __name__ == "__main__":
                 visualizationBatch(root_path= output_root, idx="", info="", data_dict=vis_dicts, 
                                        num_queries=cfg.MODEL.MASK_FORMER.NUM_OBJECT_QUERIES, save_image=True, save_segmentation=True,
                                        save_depth=True, save_ply=True,save_cloud=False)
-                # visualized_output.save(out_filename)
-            # else:
-            #     cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-            #     cv2.imshow(WINDOW_NAME, visualized_output.get_image()[:, :, ::-1])
-            #     if cv2.waitKey(0) == 27:
-            #         break  # esc to quit
-    elif args.webcam:
-        assert args.input is None, "Cannot have both --input and --webcam!"
-        assert args.output is None, "output not yet supported with --webcam!"
-        cam = cv2.VideoCapture(0)
-        for vis in tqdm.tqdm(demo.run_on_video(cam)):
-            cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-            cv2.imshow(WINDOW_NAME, vis)
-            if cv2.waitKey(1) == 27:
-                break  # esc to quit
-        cam.release()
-        cv2.destroyAllWindows()
-    elif args.video_input:
-        video = cv2.VideoCapture(args.video_input)
-        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frames_per_second = video.get(cv2.CAP_PROP_FPS)
-        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        basename = os.path.basename(args.video_input)
-        codec, file_ext = (
-            ("x264", ".mkv") if test_opencv_video_format("x264", ".mkv") else ("mp4v", ".mp4")
-        )
-        if codec == ".mp4v":
-            warnings.warn("x264 codec not available, switching to mp4v")
-        if args.output:
-            if os.path.isdir(args.output):
-                output_fname = os.path.join(args.output, basename)
-                output_fname = os.path.splitext(output_fname)[0] + file_ext
-            else:
-                output_fname = args.output
-            assert not os.path.isfile(output_fname), output_fname
-            output_file = cv2.VideoWriter(
-                filename=output_fname,
-                # some installation of opencv may not support x264 (due to its license),
-                # you can try other format (e.g. MPEG)
-                fourcc=cv2.VideoWriter_fourcc(*codec),
-                fps=float(frames_per_second),
-                frameSize=(width, height),
-                isColor=True,
-            )
-        assert os.path.isfile(args.video_input)
-        for vis_frame in tqdm.tqdm(demo.run_on_video(video), total=num_frames):
-            if args.output:
-                output_file.write(vis_frame)
-            else:
-                cv2.namedWindow(basename, cv2.WINDOW_NORMAL)
-                cv2.imshow(basename, vis_frame)
-                if cv2.waitKey(1) == 27:
-                    break  # esc to quit
-        video.release()
-        if args.output:
-            output_file.release()
-        else:
-            cv2.destroyAllWindows()
--- demo/predictor.py
+++ demo/predictor.py
@@ -51,88 +51,9 @@ class VisualizationDemo(object):
             vis_output (VisImage): the visualized image output.
         """
         vis_output = None
-        # image = image[:,:,::-1] # BGR->RGB
         predictions = self.predictor(image)
-        # Convert image from OpenCV BGR format to Matplotlib RGB format.
-        # image = image[:, :, ::-1]
-        
-        # visualizer = Visualizer(image, self.metadata, instance_mode=self.instance_mode)
-        # if "panoptic_seg" in predictions:
-        #     panoptic_seg, segments_info = predictions["panoptic_seg"]
-        #     vis_output = visualizer.draw_panoptic_seg_predictions(
-        #         panoptic_seg.to(self.cpu_device), segments_info
-        #     )
-        # else:
-        #     if "sem_seg" in predictions:
-        #         vis_output = visualizer.draw_sem_seg(
-        #             predictions["sem_seg"].argmax(dim=0).to(self.cpu_device)
-        #         )
-        #     if "instances" in predictions:
-        #         instances = predictions["instances"].to(self.cpu_device)
-        #         vis_output = visualizer.draw_instance_predictions(predictions=instances)
 
         return predictions
-
-    def _frame_from_video(self, video):
-        while video.isOpened():
-            success, frame = video.read()
-            if success:
-                yield frame
-            else:
-                break
-
-    def run_on_video(self, video):
-        """
-        Visualizes predictions on frames of the input video.
-        Args:
-            video (cv2.VideoCapture): a :class:`VideoCapture` object, whose source can be
-                either a webcam or a video file.
-        Yields:
-            ndarray: BGR visualizations of each video frame.
-        """
-        video_visualizer = VideoVisualizer(self.metadata, self.instance_mode)
-
-        def process_predictions(frame, predictions):
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            if "panoptic_seg" in predictions:
-                panoptic_seg, segments_info = predictions["panoptic_seg"]
-                vis_frame = video_visualizer.draw_panoptic_seg_predictions(
-                    frame, panoptic_seg.to(self.cpu_device), segments_info
-                )
-            elif "instances" in predictions:
-                predictions = predictions["instances"].to(self.cpu_device)
-                vis_frame = video_visualizer.draw_instance_predictions(frame, predictions)
-            elif "sem_seg" in predictions:
-                vis_frame = video_visualizer.draw_sem_seg(
-                    frame, predictions["sem_seg"].argmax(dim=0).to(self.cpu_device)
-                )
-
-            # Converts Matplotlib RGB format to OpenCV BGR format
-            vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
-            return vis_frame
-
-        frame_gen = self._frame_from_video(video)
-        if self.parallel:
-            buffer_size = self.predictor.default_buffer_size
-
-            frame_data = deque()
-
-            for cnt, frame in enumerate(frame_gen):
-                frame_data.append(frame)
-                self.predictor.put(frame)
-
-                if cnt >= buffer_size:
-                    frame = frame_data.popleft()
-                    predictions = self.predictor.get()
-                    yield process_predictions(frame, predictions)
-
-            while len(frame_data):
-                frame = frame_data.popleft()
-                predictions = self.predictor.get()
-                yield process_predictions(frame, predictions)
-        else:
-            for frame in frame_gen:
-                yield process_predictions(frame, self.predictor(frame))
 
 
 class AsyncPredictor:
